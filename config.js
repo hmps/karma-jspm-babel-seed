eaf6dbb7d06d0373e32209412083177f0a9fe02d
Hampus Persson <h@hmps.se>
2015-09-02 11:31:28 +0200

    remove angular from config, since it is not used

--- config.js
+++ config.js
@@ -14,7 +14,6 @@ System.config({
   },
 
   map: {
-    "angular": "github:angular/bower-angular@1.4.5",
     "babel": "npm:babel-core@5.8.23",
     "babel-runtime": "npm:babel-runtime@5.8.20",
     "core-js": "npm:core-js@1.1.3",
